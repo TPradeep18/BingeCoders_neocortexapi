4a4af05c503b2dcc340aa4b7b0aa936624392c25
rajan singh <rajanvsingh88@gmail.com>
2025-01-17 21:27:02 +0100

    Added running parameters in HTM and KNN

---
+++ source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
@@ -0,0 +1,44 @@
+﻿using NeoCortex;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Utility;
+using NeoCortexApi;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi.Classifiers;
+
+namespace NeoCortexApiSample
+{
+    internal class ImageReconstructionwithHtm_KNNClassifier
+    {
+        public string inputPrefix { get; private set; }
+
+        /// <summary>
+        /// Implements an experiment that demonstrates how to learn spatial patterns.
+        /// SP will learn every presented Image input in multiple iterations.
+        /// </summary>
+        public void Run()
+
+        {
+            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(ImageReconstructionwithHtm_KNNClassifier)}");
+
+            double minOctOverlapCycles = 1.0;
+            double maxBoost = 5.0;
+            // We will build a slice of the cortex with the given number of mini-columns
+            int numColumns = 64 * 64;
+
+
+            // The Size of the Image Height and width is 32 pixel
+
+            int imageSize = 32;
+            var colDims = new int[] { 64, 64 };
+
+        }
+    }
+}
+
+           
