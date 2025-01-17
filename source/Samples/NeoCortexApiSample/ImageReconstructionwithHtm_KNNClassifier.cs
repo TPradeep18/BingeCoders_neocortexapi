4e146711faae6e59019cbb829796df9966ab5f5d
ASUS <mausambhunia123@gmail.com>
2025-01-17 20:34:36 +0100

    Fuunctions added

---
+++ source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
@@ -0,0 +1,29 @@
+﻿using NeoCortex;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Utility;
+using NeoCortexApi;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi.Classifiers;
+
+namespace NeoCortexApiSample
+{
+    internal class ImageReconstructionwithHtm_KNNClassifier
+    {
+
+        /// <summary>
+        /// Implements an experiment that demonstrates how to learn spatial patterns.
+        /// SP will learn every presented Image input in multiple iterations.
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(ImageReconstructionwithHtm_KNNClassifier)}");
+
+        }
+    }
+    }
