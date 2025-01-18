600ce824ff94d8e375dae2cb254835b74100778d
ASUS <mausambhunia123@gmail.com>
2025-01-18 21:51:50 +0100

    HTM Classifier

--- source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
+++ source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
@@ -15,18 +15,90 @@ namespace NeoCortexApiSample
 {
     internal class ImageReconstructionwithHtm_KNNClassifier
     {
+        public string inputPrefix { get; private set; }
 
         /// <summary>
         /// Implements an experiment that demonstrates how to learn spatial patterns.
         /// SP will learn every presented Image input in multiple iterations.
         /// </summary>
+        public void Run() //Mausam 17/01
 
-        //Implementing the HTM & KNN Classifier
-
-        public void Run() 
         {
             Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(ImageReconstructionwithHtm_KNNClassifier)}");
 
+            double minOctOverlapCycles = 1.0;
+            double maxBoost = 5.0;
+            // We will build a slice of the cortex with the given number of mini-columns
+            int numColumns = 64 * 64;
+
+
+            // The Size of the Image Height and width is 32 pixel
+
+            int imageSize = 32;
+            var colDims = new int[] { 64, 64 };
+
+            // This is a set of configuration parameters used in the experiment.
+            HtmConfig cfg = new HtmConfig(new int[] { imageSize, imageSize }, new int[] { numColumns })
+            {
+                CellsPerColumn = 10,
+                InputDimensions = new int[] { imageSize, imageSize },
+                NumInputs = imageSize * imageSize,
+                ColumnDimensions = colDims,
+                MaxBoost = maxBoost,
+                DutyCyclePeriod = 100,
+                MinPctOverlapDutyCycles = minOctOverlapCycles,
+                GlobalInhibition = false,
+                NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                PotentialRadius = (int)(0.15 * imageSize * imageSize),
+                LocalAreaDensity = -1,
+                ActivationThreshold = 10,
+                MaxSynapsesPerSegment = (int)(0.01 * numColumns),
+                Random = new ThreadSafeRandom(42),
+                StimulusThreshold = 10,
+            };
+
+            //Runnig the Experiment
+            var sp = RunExperimentWithHTMClassifier(cfg, inputPrefix);
+
         }
-    }
+
+        /// <summary>
+        /// Implements the experiment.
+        /// </summary>
+        /// <param name="cfg"></param>
+        /// <param name="inputPrefix"> The name of the images</param>
+        /// <returns>The trained bersion of the SP.</returns>
+        private (SpatialPooler, HtmClassifier<string, int[]>) RunExperimentWithHTMClassifier(HtmConfig cfg, string inputPrefix)
+        {
+            var mem = new Connections(cfg);
+            bool isInStableState = false;
+
+            int numColumns = 64 * 64;
+            string trainingFolder = "Sample\\TestFiles";
+            var trainingImages = Directory.GetFiles(trainingFolder, $"{inputPrefix}*.png");
+            Debug.WriteLine($"File is acknowledged");
+            int imgSize = 32;
+            string testName = "test_image";
+
+            HomeostaticPlasticityController hpa = new HomeostaticPlasticityController(mem, trainingImages.Length * 50, (isStable, numPatterns, actColAvg, seenInputs) =>
+            {
+                isInStableState = isStable;
+                Debug.WriteLine(isStable ? "Entered STABLE state." : "INSTABLE STATE.");
+            }, requiredSimilarityThreshold: 0.975);
+
+            SpatialPooler sp = new SpatialPooler(hpa);
+            sp.Init(mem, new DistributedMemory() { ColumnDictionary = new InMemoryDistributedDictionary<int, NeoCortexApi.Entities.Column>(1) });
+
+            HtmClassifier<string, int[]> classifier = new HtmClassifier<string, int[]>();
+
+            int[] activeArray = new int[numColumns];
+            int maxCycles = 5;
+            int currentCycle = 0;
+
+            while (!isInStableState && currentCycle < maxCycles)
+            {
+                foreach (var image in trainingImages)
+                { }
+            }
+        }
     }
