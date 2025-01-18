72ca0e51e7c871e4d8026c87c415699fc69fc146
rajan singh <rajanvsingh88@gmail.com>
2025-01-18 13:51:44 +0100

    HTM CLassifier

--- source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
+++ source/Samples/NeoCortexApiSample/ImageReconstructionwithHtm_KNNClassifier.cs
@@ -21,7 +21,7 @@ namespace NeoCortexApiSample
         /// Implements an experiment that demonstrates how to learn spatial patterns.
         /// SP will learn every presented Image input in multiple iterations.
         /// </summary>
-        public void Run()
+        public void Run() //Mausam 17/01
 
         {
             Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(ImageReconstructionwithHtm_KNNClassifier)}");
@@ -32,13 +32,61 @@ namespace NeoCortexApiSample
             int numColumns = 64 * 64;
 
 
-            // The Size of the Image Height and width is 32 pixel to maintain the aspect ratio
+            // The Size of the Image Height and width is 32 pixel
 
             int imageSize = 32;
             var colDims = new int[] { 64, 64 };
 
+            // This is a set of configuration parameters used in the experiment.
+            HtmConfig cfg = new HtmConfig(new int[] { imageSize, imageSize }, new int[] { numColumns })
+            {
+                CellsPerColumn = 10,
+                InputDimensions = new int[] { imageSize, imageSize },
+                NumInputs = imageSize * imageSize,
+                ColumnDimensions = colDims,
+                MaxBoost = maxBoost,
+                DutyCyclePeriod = 100,
+                MinPctOverlapDutyCycles = minOctOverlapCycles,
+                GlobalInhibition = false,
+                NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                PotentialRadius = (int)(0.15 * imageSize * imageSize),
+                LocalAreaDensity = -1,
+                ActivationThreshold = 10,
+                MaxSynapsesPerSegment = (int)(0.01 * numColumns),
+                Random = new ThreadSafeRandom(42),
+                StimulusThreshold = 10,
+            };
+
+        }
+
+        /// <summary>
+        /// Implements the experiment.
+        /// </summary>
+        /// <param name="cfg"></param>
+        /// <param name="inputPrefix"> The name of the images</param>
+        /// <returns>The trained bersion of the SP.</returns>
+        private (SpatialPooler, HtmClassifier<string, int[]>) RunExperimentWithHTMClassifier(HtmConfig cfg, string inputPrefix)
+        {
+            var mem = new Connections(cfg);
+            bool isInStableState = false;
+
+            int numColumns = 64 * 64;
+            string trainingFolder = "Sample\\TestFiles";
+            var trainingImages = Directory.GetFiles(trainingFolder, $"{inputPrefix}*.png");
+            Debug.WriteLine($"File is acknowledged");
+            int imgSize = 32;
+            string testName = "test_image";
+
+
+            //
+            HomeostaticPlasticityController hpa = new HomeostaticPlasticityController(mem, trainingImages.Length * 50, (isStable, numPatterns, actColAvg, seenInputs) =>
+            {
+                isInStableState = isStable;
+                Debug.WriteLine(isStable ? "Entered STABLE state." : "INSTABLE STATE.");
+            }, requiredSimilarityThreshold: 0.975);
+
+            SpatialPooler sp = new SpatialPooler(hpa);
+            sp.Init(mem, new DistributedMemory() { ColumnDictionary = new InMemoryDistributedDictionary<int, NeoCortexApi.Entities.Column>(1) });
         }
     }
 }
-
-           
